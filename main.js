4b78d944f30a8c43c99606ce7a3de83337319608
Tou Yang <87285827+ToodlesMrPoodle@users.noreply.github.com>
2022-04-24 21:22:32 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,97 @@
+<script type="module">
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+// Setup
+
+const scene = new THREE.Scene();
+</script>
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+camera.position.setX(-3);
+
+renderer.render(scene, camera);
+
+// Lights
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
+
+// Background
+
+const spaceTexture = new THREE.TextureLoader().load('public/black.jpg');
+scene.background = spaceTexture;
+
+// Moon
+
+const earthTexture = new THREE.TextureLoader().load('public/earth.jpg');
+const normalTexture = new THREE.TextureLoader().load('public/earthnormal.jpg');
+
+const earths = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: earthTexture,
+    normalMap: normalTexture,
+  })
+);
+
+scene.add(earths);
+
+earths.position.z = 2;
+earths.position.setX(5);
+
+// Scroll Animation
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+  earths.rotation.x += 0;
+  earths.rotation.y += 0.075;
+  earths.rotation.z += 0;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.rotation.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
+
+// Animation Loop
+
+function animate() {
+  requestAnimationFrame(animate);
+  earths.rotation.y += 0.005;
+
+  // controls.update();
+
+  renderer.render(scene, camera);
+}
+
+animate();
