905ce46205339b18a8e1aa79e6e1fbe60a522b4a
Tou Yang <87285827+ToodlesMrPoodle@users.noreply.github.com>
2022-05-11 08:52:39 -0500

    Update main.js

--- main.js
+++ main.js
@@ -110,8 +110,8 @@ const cube = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicM
 scene.add(cube);
 
 cube.position.z = 56
-cube.position.x = -2
-cube.rotation.y = 6
+cube.position.x = -5
+cube.rotation.y = 5
 
 // Scroll Animation
 
